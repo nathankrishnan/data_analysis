54a001af3b4a779b0064872c4e4e938b1726fdb9
Nathan <nathank@gwu.edu>
2015-09-24 23:59:44 -0400

    numpy for helping load csv data

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import csv
+import numpy
 
 # default delimeter is tab but can be changed by passing in an optional paramter for default
 def open_csv(filename, default='\t'):
@@ -11,3 +12,28 @@ def open_csv(filename, default='\t'):
 
 data_from_csv = open_csv('data.csv')
 print(data_from_csv[0])
+
+FIELDNAMES = ['', 'id', 'priceLabel', 'name', 'brandId', 'brandName', 'imageLink', 'desc', 'vendor', 'pattern', 'material']
+
+# 'b' boolean
+# 'i' (signed) integer
+# 'u' unsigned integer
+# 'f' floating-point
+# 'c' complex-floating point
+# 'O' (Python) objects
+# 'S', 'a'    (byte-)string
+# 'U' Unicode
+# 'V' raw data (void)
+
+DATATYPES = [('myint', 'i'), ('myid', 'i'), ('price', 'f8'), ('name', 'a200'),
+             ('brandId', '<i8'), ('brandName', 'a200'), ('imageUrl', '|S500'), 
+             ('description', '|S900'),  ('vendor', '|S100'),  ('pattern', '|S50'),  ('material', '|S50'), ]
+
+
+
+def load_data(filename, default='\t'):
+	# Skip the header line of the file, which is line one
+	my_csv = numpy.genfromtxt(filename, delimiter=default, skip_header=1, invalid_raise=False, names=FIELDNAMES, dtype=DATATYPES)
+	return my_csv
+
+my_csv = load_data('data.csv')
